148195cd522206eb4499bf2ffb5116d6cd3523aa
所无聊 <2409277719@qq.com>
2022-10-03 15:23:12 +0800

    侧边栏生效

---
+++ .dumi/theme/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Layout from 'dumi-theme-default/es/layout';
+import './layout.less';
+
+export default ({ children, ...props }) => (
+  <Layout {...props}>
+    <>{children}</>
+  </Layout>
+);
