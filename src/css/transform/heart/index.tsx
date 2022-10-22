0e7dd9282a67892be22210b6172e54ad4114229e
所无聊 <2409277719@qq.com>
2022-10-22 20:42:14 +0800

    更新心形图案

---
+++ src/css/transform/heart/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.less';
+
+const Heart: React.FC = () => {
+  return (
+    <div className="container">
+      <div className="heart-shape"></div>
+    </div>
+  );
+};
+
+export default Heart;
