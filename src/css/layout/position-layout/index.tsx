d7ef9bcabfe0165447e9e89cc02a1695bfe4dc2c
所无聊 <2409277719@qq.com>
2022-11-13 19:35:14 +0800

    更新需要多个 position 的案例

---
+++ src/css/layout/position-layout/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.less';
+
+function PositionLayout() {
+  return (
+    <div className="position-demo">
+      <div className="parent">
+        <div className="child">
+          <div className="g-child"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PositionLayout;
